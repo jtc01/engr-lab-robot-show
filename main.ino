c451b3f064eaa4e6507a26d93191fedc670a13c7
Josh Cao <joshcaopdx@gmail.com>
2026-03-14 13:04:49 -0700

    Added winning and losing melodies

--- main.ino
+++ main.ino
@@ -2,6 +2,16 @@
 //
 #include <Servo.h> // include the Servo library
 
+#define NOTE_G4  392
+#define NOTE_C5  523
+#define NOTE_E5  659
+#define NOTE_G5  784
+#define NOTE_B4  494
+#define NOTE_AS4 466
+#define NOTE_A4  440
+#define NOTE_GS4 415
+
+
 Servo myservo; //create the servo object
 int pos=0; //variable to store the servo position
 bool bat=false;
@@ -12,6 +22,11 @@ const int outEcho = 9;
 const int buzzer = 13;
 long duration;
 float distanceCm;
+
+int chargeMelody[]    = { NOTE_G4, NOTE_C5, NOTE_E5, NOTE_G5, NOTE_E5, NOTE_G5 };
+int chargeDurations[] = { 150,     150,     150,      300,     150,     500     };
+int tromboneMelody[]    = { NOTE_B4, NOTE_AS4, NOTE_A4, NOTE_GS4 };
+int tromboneDurations[] = { 300,     300,      300,      900      };
 
 
 
@@ -64,10 +79,7 @@ void detectRun() {
   
   if (distanceCm < 30){
     Serial.println("Home Run!");
-    tone(buzzer, 1000);
-    delay(500);
-
-    noTone(buzzer);
+    charge();
     delay(500);
     bat=false;
   }
@@ -85,6 +97,23 @@ void detectOut() {
   
   if (distanceCm < 30){
     Serial.println("Out!");
+    trombone();
     bat=false;
   }
+}
+
+void charge(){
+  for (int i = 0; i < 6; i++) {
+    tone(buzzer, chargeMelody[i], chargeDurations[i]);
+    delay(chargeDurations[i] + 30);
+  }
+  noTone(buzzer);
+}
+
+void trombone(){
+  for (int i = 0; i < 4; i++) {
+    tone(buzzer, tromboneMelody[i], tromboneDurations[i]);
+    delay(tromboneDurations[i] + 30);
+  }
+  noTone(buzzer);
 }
